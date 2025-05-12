2da8af261d96c940e489e00f2f2a710c166a70bd
Phuc Nguyen <phucnguyen@Phucs-MacBook-Pro.local>
2025-05-12 22:39:07 +0700

    fix import issue

--- apis/models/chat.py
+++ apis/models/chat.py
@@ -3,9 +3,9 @@ from typing import Optional, List
 
 class ChatGenerationModel(BaseModel):
     query: str = Field(..., description="User's current question or search query")
-    search_type: Optional[str] = Field(default="keyword_search", description="Search method: keyword_search, semantic_search, or hybrid_search")
+    search_type: Optional[str] = Field(default="hybrid", description="Search method: keyword_search, semantic_search, or hybrid_search")
     semantic_ratio: Optional[float] = Field(default=0.5, description="Semantic weight for hybrid search (0 to 1)")
     embedder: Optional[str] = Field(default="embedder", description="Embedding model name")
     top_k: Optional[int] = Field(default=3, description="Number of results to retrieve from search")
-    index: Optional[str] = Field(default="vns_policy", description="Target index to search")
+    index: Optional[str] = Field(default="agri_chatbot_v2", description="Target index to search")
     history: Optional[List[str]] = Field(default_factory=list, description="Optional list of previous user messages")
--- core/logics/chat.py
+++ core/logics/chat.py
@@ -1,6 +1,6 @@
 import json
 from fastapi import HTTPException
-from core.clients.groq import GroqClient as model_name
+from core.clients.groq import GroqClient as client, model_name
 from core.clients.meilisearch import MeiliClient as meilisearch_client
 from apis.v1.meilisearch_router import semantic_search, hybrid_search, keyword_search
 from core.agents.prompts.content import AnswerGeneration, NextQuestionGeneration
@@ -36,6 +36,8 @@ def generate_next_question(request: ChatGenerationModel, client: object):
     context = "\n\n".join(hit["content"] for hit in context_data["hits"])
 
     prompt_content = NextQuestionGeneration.create_message_content(context, request.query, request.history)
+    print(client)
+    print(model_name, "*"*100)
     completion = client.chat.completions.create(
         model=model_name,
         messages=[{"role": "user", "content": prompt_content}]
